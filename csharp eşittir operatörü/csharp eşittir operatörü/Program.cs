a95bce0634955ceec870ef8ec2bbaf41d6de1612
Okan Çelebi <okancelebi06@gmail.com>
2023-02-03 18:10:54 +0300

    Add files via upload

---
+++ csharp eşittir operatörü/csharp eşittir operatörü/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_eşittir_operatörü
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Eşittir operatörü : ==
+            int sayi1 = 4;
+            int sayi2 = 4;
+            bool kontrolet = sayi1 == sayi2;
+            Console.WriteLine(kontrolet);
+            Console.ReadLine();
+        }
+    }
+}
